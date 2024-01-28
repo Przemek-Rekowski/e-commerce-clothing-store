c363382e329ed241860b44922e899a5e89fbdf73
Przemysław Rekowski <rekowskiprzemyslaw03@gmail.com>
2024-01-28 14:29:12 +0100

    Inventory validation added

---
+++ Application/Inventory/CreateInventory/CreateInventoryHandlerValidator.cs
@@ -0,0 +1,17 @@
+﻿using Application.Inventory.CreateInventory;
+using FluentValidation;
+
+namespace EcommerceShop.Application.Inventory.CreateInventory
+{
+    public class CreateInventoryCommandValidator : AbstractValidator<CreateInventoryCommand>
+    {
+        public CreateInventoryCommandValidator()
+        {
+            RuleFor(i => i.Size)
+                .NotEmpty();
+
+            RuleFor(i => i.Quantity)
+                .GreaterThanOrEqualTo(0);
+        }
+    }
+}
--- Application/Inventory/Dtos/InventoryDto.cs
+++ Application/Inventory/Dtos/InventoryDto.cs
@@ -4,6 +4,6 @@
     {
         public int ProductId { get; set; }
         public string Size { get; set; }
-        public string Quantity { get; set; }
+        public int Quantity { get; set; }
     }
 }
