3629a3bb91a4f2e1a66fbbc886445abc308afdbc
Przemysław Rekowski <rekowskiprzemyslaw03@gmail.com>
2024-03-12 22:45:00 +0100

    routing feature

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css';
 import Navbar from './assets/components/Navbar/Navbar.jsx';
 import Footer from './assets/components/Footer/Footer.jsx';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './assets/pages/Home.jsx'
+import About from './assets/pages/About.jsx'
+import Contact from './assets/pages/Contact.jsx'
+import Shop from './assets/pages/Shop.jsx'
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Footer />
-      </div>
-    </Router>
+    <div>
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={Home()} />
+            <Route path="/about" element={About()} />
+            <Route path="/contact" element={Contact()} />
+            <Route path="/shop" element={Shop()} />
+          </Routes>
+          <Footer/>
+       </Router>
+    </div>
   );
 }
 
