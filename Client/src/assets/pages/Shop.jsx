5d4b68002dcfd7a8cfb6edc8e4b2f3cae9b3529d
Przemysław Rekowski <rekowskiprzemyslaw03@gmail.com>
2024-04-14 21:50:13 +0200

    product fetch added

--- Client/src/assets/pages/Shop.jsx
+++ Client/src/assets/pages/Shop.jsx
@@ -1,9 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import ProductCard from '../../assets/components/ProductCard/ProductCard.jsx';
+import axios from 'axios';
 
-function Shop() {
+const Products = () => {
+  const [productsData, setProductsData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://localhost:7172/api");
+        setProductsData(response.data);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div>Shop</div>
+    <div>
+      {productsData.map((product, index) => (
+        <ProductCard key={index} product={product} />
+      ))}
+    </div>
   );
 }
 
-export default Shop;
+export default Products;
