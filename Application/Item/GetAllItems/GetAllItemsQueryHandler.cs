92cbbab9182bf8ed10ab447cac7c2135e1df855e
Przemysław Rekowski <rekowskiprzemyslaw03@gmail.com>
2024-04-26 21:04:44 +0200

    pagination in product items

--- Application/Item/GetAllItems/GetAllItemsQueryHandler.cs
+++ Application/Item/GetAllItems/GetAllItemsQueryHandler.cs
@@ -1,28 +1,23 @@
 ﻿using AutoMapper;
+using Domain.Constants;
 using Domain.Interfaces.Product;
 using EcommerceShop.Application.Item.Dtos;
 using MediatR;
 
 namespace EcommerceShop.Application.Item.GetAllItems
 {
-    public class GetAllItemsQueryHandler : IRequestHandler<GetAllItemsQuery, IEnumerable<ItemDto>>
+    public class GetAllItemsQueryHandler(IMapper mapper,
+        IProductItemRepository itemRepository) : IRequestHandler<GetAllItemsQuery, PagedResult<ItemDto>>
     {
-        private readonly IProductItemRepository _itemRepository;
-        private readonly IMapper _mapper;
-
-        public GetAllItemsQueryHandler(IProductItemRepository itemRepository, IMapper mapper)
+        public async Task<PagedResult<ItemDto>> Handle(GetAllItemsQuery request, CancellationToken cancellationToken)
         {
-            _itemRepository = itemRepository;
-            _mapper = mapper;
-        }
+            var (items, totalCount) = await itemRepository.GetAll(request.SearchPhrase,
+                request.PageSize,
+                request.PageNumber);
+            var itemDtos = mapper.Map<IEnumerable<ItemDto>>(items);
 
-        public async Task<IEnumerable<ItemDto>> Handle(GetAllItemsQuery request, CancellationToken cancellationToken)
-        {
-            var items = await _itemRepository.GetAll();
-            var dtos = _mapper.Map<IEnumerable<ItemDto>>(items);
-
-
-            return dtos;
+            var result = new PagedResult<ItemDto>(itemDtos, totalCount, request.PageSize, request.PageNumber);
+            return result;
         }
     }
 }
